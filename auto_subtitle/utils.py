fac96c51e247ff5d0020c07da234726196a7744b
Cedric Murphy <43464126+Andalite1999@users.noreply.github.com>
2023-08-13 16:55:17 -0700

    increased split size

--- auto_subtitle/utils.py
+++ auto_subtitle/utils.py
@@ -2,7 +2,7 @@ from typing import Iterator, TextIO
 import os
 # os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:10"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:25"
 
 
 def str2bool(string):
