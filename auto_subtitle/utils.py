f9111fcaa95bc1a0481b2b2257b53fe80af8b186
Cedric Murphy <43464126+Andalite1999@users.noreply.github.com>
2023-08-13 16:51:21 -0700

    try again to reduce vram

--- auto_subtitle/utils.py
+++ auto_subtitle/utils.py
@@ -1,8 +1,8 @@
 from typing import Iterator, TextIO
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = ""
+# os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
-# os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:10"
 
 
 def str2bool(string):
